7b070c7ca63b7a1b0e31e6f9c781c0aeb569ea3f
FJCon <conilfernando@gmail.com>
2023-10-21 19:12:16 -0300

    cambios

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const buttonForm = document.querySelector("#formGoogle");
+const buttonUbicacion = document.querySelector("#ubicacion")
+
+buttonForm.addEventListener("click",(e)=>{
+    let ventana = window.open("https://forms.gle/Huj4FgwJ1dgkWVgt8","_blank")
+})
+
+buttonUbicacion.addEventListener("click",(e)=>{
+    let ventana = window.open("https://maps.app.goo.gl/cwxKdMbJmg7kBgF39","_blank")
+})
