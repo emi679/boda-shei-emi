a61fbb3a70ecdeff6c7d43e1f8c2d29407782f17
FJCon <conilfernando@gmail.com>
2023-10-21 20:24:15 -0300

    carrucel

--- js/script.js
+++ js/script.js
@@ -8,3 +8,49 @@ buttonForm.addEventListener("click",(e)=>{
 buttonUbicacion.addEventListener("click",(e)=>{
     let ventana = window.open("https://maps.app.goo.gl/cwxKdMbJmg7kBgF39","_blank")
 })
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+ showSlides(slideIndex = n);
+}
+
+function currentSlide(n) {
+ showSlides(slideIndex = n);
+}
+
+let slideInterval = setInterval(() => {
+ plusSlides(1);
+}, 3000);
+
+let slideshowContainer = document.getElementsByClassName('slideshow-container')[0];
+slideshowContainer.addEventListener('mouseenter', () => {
+ clearInterval(slideInterval);
+});
+
+slideshowContainer.addEventListener('mouseleave', () => {
+ slideInterval = setInterval(() => {
+ plusSlides(1);
+ }, 3000);
+});
+
+function showSlides(n) {
+ let i;
+ let slides = document.getElementsByClassName("mySlides");
+ let dots = document.getElementsByClassName("demo");
+ let captionText = document.getElementById("caption");
+ if (n > diapositivas.length) {slideIndex = 1}
+ if (n < 1) {slideIndex = slides.length}
+ for (i = 0; i < slides.length; i ) {
+ slides<x><x>[i]</x></x>.style.display = "none";
+ }
+ for (i = 0; i < dots.length; i ) {
+ dots<x><x>[i]</x></x>.className = dots<x><x>[i]</x></x>.className.replace(" active", "");
+ }
+ slides[slideIndex-1].style.display = "block";
+ dots[slideIndex-1].className = " active";
+ captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+  
