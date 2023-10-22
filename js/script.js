40804c27627e2889f76aadd73fc01631d4bbeb2b
FJCon <conilfernando@gmail.com>
2023-10-22 17:03:04 -0300

    slider manual terminado

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 const buttonForm = document.querySelector("#formGoogle");
-const buttonUbicacion = document.querySelector("#ubicacion")
+const buttonUbicacion = document.querySelector("#ubicacion");
+
+const buttonNext = document.querySelector("#btn_right");
+const buttonPrev = document.querySelector("#btn_left");
+const slider = document.querySelector(".slider")
+
+
+//Funciones para ubicacacion y formulario
 
 buttonForm.addEventListener("click",(e)=>{
     let ventana = window.open("https://forms.gle/Huj4FgwJ1dgkWVgt8","_blank")
@@ -9,48 +16,43 @@ buttonUbicacion.addEventListener("click",(e)=>{
     let ventana = window.open("https://maps.app.goo.gl/cwxKdMbJmg7kBgF39","_blank")
 })
 
-let slideIndex = 1;
-showSlides(slideIndex);
+//Funciones para slider de fotos
 
-function plusSlides(n) {
- showSlides(slideIndex = n);
+let index = 0;
+
+function mostrarFoto(){
+    switch(index){
+        case -1:
+            index = 3;
+            slider.style.marginLeft = "-300%";
+            break
+        case 0:
+            slider.style.marginLeft = "0%";
+            break;
+        case 1:
+            slider.style.marginLeft = "-100%";
+            break;
+        case 2:
+            slider.style.marginLeft = "-200%";
+            break;
+        case 3:
+            slider.style.marginLeft = "-300%";
+            break;
+        case 4:
+            index = 0;
+            slider.style.marginLeft = "0%";
+    }
 }
 
-function currentSlide(n) {
- showSlides(slideIndex = n);
-}
+buttonNext.addEventListener("click",()=>{
+    index +=1;
+    console.log(index)
+    mostrarFoto();
 
-let slideInterval = setInterval(() => {
- plusSlides(1);
-}, 3000);
+})
 
-let slideshowContainer = document.getElementsByClassName('slideshow-container')[0];
-slideshowContainer.addEventListener('mouseenter', () => {
- clearInterval(slideInterval);
+buttonPrev.addEventListener("click",()=>{
+    index -=1;
+    console.log(index);
+     mostrarFoto();
 });
-
-slideshowContainer.addEventListener('mouseleave', () => {
- slideInterval = setInterval(() => {
- plusSlides(1);
- }, 3000);
-});
-
-function showSlides(n) {
- let i;
- let slides = document.getElementsByClassName("mySlides");
- let dots = document.getElementsByClassName("demo");
- let captionText = document.getElementById("caption");
- if (n > diapositivas.length) {slideIndex = 1}
- if (n < 1) {slideIndex = slides.length}
- for (i = 0; i < slides.length; i ) {
- slides<x><x>[i]</x></x>.style.display = "none";
- }
- for (i = 0; i < dots.length; i ) {
- dots<x><x>[i]</x></x>.className = dots<x><x>[i]</x></x>.className.replace(" active", "");
- }
- slides[slideIndex-1].style.display = "block";
- dots[slideIndex-1].className = " active";
- captionText.innerHTML = dots[slideIndex-1].alt;
-}
-
-  
