9e82a7e8b1c3102ce041a033b053283fbb0f465c
FJCon <conilfernando@gmail.com>
2023-10-24 17:18:11 -0300

    cuenta regresiva funcional

---
+++ js/contador.js
@@ -0,0 +1,31 @@
+const DATE_TARGET = new Date('03/23/2024 8:00 PM');
+
+const SPAN_DAYS = document.querySelector("#days > h3");
+const SPAN_HOURS = document.querySelector("#hours > div > h3");
+const SPAN_MINUTES = document.querySelector("#minutes > div > h3");
+const SPAN_SECONDS = document.querySelector("#seconds > div > h3");
+
+const MILLISECONDS_OF_A_SECOND = 1000;
+const MILLISECONDS_OF_A_MINUTE = MILLISECONDS_OF_A_SECOND * 60;
+const MILLISECONDS_OF_A_HOUR = MILLISECONDS_OF_A_MINUTE * 60;
+const MILLISECONDS_OF_A_DAY = MILLISECONDS_OF_A_HOUR * 24
+
+function updateCountdown() {
+    // Calcs
+    const NOW = new Date()
+    const DURATION = DATE_TARGET - NOW;
+
+    const REMAINING_DAYS = Math.floor(DURATION / MILLISECONDS_OF_A_DAY);
+    const REMAINING_HOURS = Math.floor((DURATION % MILLISECONDS_OF_A_DAY) / MILLISECONDS_OF_A_HOUR);
+    const REMAINING_MINUTES = Math.floor((DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE);
+    const REMAINING_SECONDS = Math.floor((DURATION % MILLISECONDS_OF_A_MINUTE) / MILLISECONDS_OF_A_SECOND);
+
+    SPAN_DAYS.textContent = REMAINING_DAYS;
+    SPAN_HOURS.textContent = REMAINING_HOURS;
+    SPAN_MINUTES.textContent = REMAINING_MINUTES;
+    SPAN_SECONDS.textContent = REMAINING_SECONDS;
+}
+
+updateCountdown();
+
+setInterval(updateCountdown, MILLISECONDS_OF_A_SECOND);
